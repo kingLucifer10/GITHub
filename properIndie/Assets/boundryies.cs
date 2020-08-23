0c2251b8a07d3258d74889d17788aedd7e9ec23a
Mayur Mansing Nangare <mayurnangare10@gmail.com>
2020-08-23 10:34:12 +0530

    INDIE1.0

---
+++ properIndie/Assets/boundryies.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class boundryies : MonoBehaviour
+{
+    public Camera g_mainCamera;
+    private UnityEngine.Vector2 g_screenBoundries;
+    private float g_objectWidth;
+    private float g_objectHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        g_screenBoundries = g_mainCamera.ScreenToWorldPoint(new UnityEngine.Vector3(Screen.width, Screen.height, g_mainCamera.transform.position.z));
+        g_objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x / 2;
+        g_objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y / 2;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        UnityEngine.Vector3 l_viewPosition = transform.position;
+
+        l_viewPosition.x = Mathf.Clamp(l_viewPosition.x, g_screenBoundries.x * -1 + g_objectWidth, g_screenBoundries.x * - g_objectWidth);
+        l_viewPosition.y = Mathf.Clamp(l_viewPosition.y, g_screenBoundries.y * -1 + g_objectHeight, g_screenBoundries.y * - g_objectHeight);
+        transform.position = l_viewPosition;
+    }
+
+}
+
---
+++ properIndie/Assets/joyStick.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class joyStick : MonoBehaviour
+{
+    public Transform g_player;
+    public float g_speed = 5.0f;
+
+    private bool g_touchStart = false;
+    private Vector2 g_pointA;
+    private Vector2 g_pointB;
+
+    public Transform g_innerCircle;
+    public Transform g_outerCircle;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            g_pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+
+            g_innerCircle.transform.position = g_pointA * -1;
+            g_outerCircle.transform.position = g_pointA * -1;
+            g_innerCircle.GetComponent<SpriteRenderer>().enabled = true;
+            g_outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+            g_touchStart = true;
+            g_pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else
+        {
+            g_touchStart = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (g_touchStart)
+        {
+            Vector2 l_offset = g_pointB - g_pointA;
+            Vector2 l_direction = Vector2.ClampMagnitude(l_offset, 1.0f);
+            f_moveCharacter(l_direction * 1);
+
+            g_innerCircle.transform.position = new Vector2(g_pointA.x + l_direction.x, g_pointA.y + l_direction.y)*-1;
+        }
+        else
+        {
+            g_innerCircle.GetComponent<SpriteRenderer>().enabled = false;
+            g_outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+    void f_moveCharacter(Vector2 l_direction)
+    {
+        g_player.Translate(l_direction * g_speed * Time.deltaTime); 
+    }
+}
